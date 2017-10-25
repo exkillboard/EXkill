b8de2a76d33c5f85954f6e913454a994b9efe140
Thomas Depierre <thomas.depierre@ascential.com>
2017-10-25 23:16:04 +0100

    works on pager, not finished

--- apps/exkill_web/lib/exkill_web/controllers/page_controller.ex
+++ apps/exkill_web/lib/exkill_web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule ExkillWeb.PageController do
   use ExkillWeb, :controller
 
   def index(conn, _params) do
-    render conn, "index.html", page_title: "Home", top_isk: %{}, sponsored: []
+    render conn, "index.html", page_title: "Home", top_isk: %{}, sponsored: [], kills: []
   end
 end
